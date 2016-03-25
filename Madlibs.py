3df196d8a2450ef5f689fc30d155836846d41507
Garett <GarettMortensen@GMail.com>
2016-03-25 11:34:30 -0400

    Update Madlibs.py

--- Madlibs.py
+++ Madlibs.py
@@ -24,6 +24,6 @@ year = input("Enter a year: ")
 
 
 #The template for the story
-STORY = "This morning I woke up and felt %s because %s was going to finally %s over the big %s %s. On the other side of the %s were many %ss protesting to keep %s in stores. The crowd began to %s to the rythym of the %s, which made all of the %ss very %s. %s tried to %s into the sewers and found %s rats. Needing help, %s quickly called %s. %s appeared and saved %s by flying to %s and dropping %s into a puddle of %s. %s then fell asleep and woke up in the year %s, in a world where %ss ruled the world."
+STORY = "This morning I woke up and felt {} because {} was going to finally {} over the big {} {}. On the other side of the {} were many {}s protesting to keep {} in stores. The crowd began to {} to the rythym of the {}, which made all of the {}s very {}. {} tried to {} into the sewers and found {} rats. Needing help, {} quickly called {}. {} appeared and saved {} by flying to {} and dropping {} into a puddle of {}. {} then fell asleep and woke up in the year {}, in a world where {}s ruled the world."
 
 print STORY % (adj1, name, verb1, adj2, noun1, noun2, animal, food, verb2, noun3, fruit, adj3, name, verb3, number, name, hero, hero, name, country, name, dessert, name, year, noun4)
